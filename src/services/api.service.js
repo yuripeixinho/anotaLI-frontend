8b6591d05e269d7564b18efa59451c57bb4c18b3
Yago <peixinhoyago@gmail.com>
2024-11-11 23:28:59 -0300

    feat: Favicon da aplicação

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { performLogout } from "../pages/auth/logout";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://localhost:44353/",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5022/",
   headers: {
     "Content-Type": "application/json",
   },
